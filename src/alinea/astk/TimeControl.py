44980fb1c0ef46e980953ab61ebe21b381bc4541
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2013-05-28 16:12:06 +0000

    make TimeControler manipulate a dict of Time control

--- src/alinea/astk/TimeControl.py
+++ src/alinea/astk/TimeControl.py
@@ -13,6 +13,11 @@ class TimeControlSet:
         if not hasattr(self,attname):
             setattr(self,attname,defaultvalue)
 
+            
+def simple_delay_timing(delay = 1, steps =1):
+    return (TimeControlSet(dt=delay) if not i % delay  else TimeControlSet(dt=0) for i in range(steps))
+            
+            
 class TimeControl:
 
     def __init__(self, delay=1, steps=1, model=None, weather=None):
@@ -23,41 +28,37 @@ class TimeControl:
         self.weather = weather
         
         try:
-            self._timing = model.get_timing(delay=delay, steps = steps, model = model, weather = weather)
+            self._timing = model.timing(delay=delay, steps = steps, weather = weather)
         except:
-            self._timing = (TimeControlSet(dt=delay) if not i % delay  else TimeControlSet(dt=0) for i in range(steps)) # a generator of timecontrolset objects to be used during a simulation
+            self._timing =  simple_delay_timing(delay=delay, steps=steps)# a generator of timecontrolset objects to be used during a simulation
 
     def __iter__(self):
-        return self
+        return TimeControl(delay = self.delay, steps = self.steps, model = self.model, weather = self.weather) 
     
     def next(self):
         return self._timing.next()
-        
-    def reset(self):
-        new = TimeControl(delay = self.delay, steps = self.steps, model = self.model, weather = self.weather) 
-        self._timing = new._timing
-        
-    def check_dt(self):
-        l = [tc.dt for tc in self._timing]
-        self.reset()
-        return l
                   
             
 class TimeControler:
 
-    def __init__(self, TimeControl_list):
+    def __init__(self, **kwd):
         """ create a controler for parallel run of time controls
             Allows to emulate 'discrete event'-like evaluation of timecontrol objects in a script
         """
-        self._timelist = TimeControl_list
+        self._timedict = dict(kwd)
+        self.args = dict(kwd)
+        self.numiter = 0
         
     def __iter__(self):
+        self._timedict = dict((k,iter(v)) for k,v in self._timedict.iteritems())
+        self.numiter = 0
         return self
     
     def next(self):
-        return map(next, self._timelist)
+        d = dict((k,v.next()) for k,v in self._timedict.iteritems())
+        if len(d) == 0:
+            raise StopIteration
+        self.numiter += 1
+        return d
         
-    def reset(self):
-        map(lambda(x): x.reset(), self._timelist)
-
     
