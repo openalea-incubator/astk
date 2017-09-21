d46124875752ca9e3852a13fe8d755236830fc61
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2017-09-21 14:47:43 +0200

    update test for sun_extraradiation

--- test/test_sun_position.py
+++ test/test_sun_position.py
@@ -1,8 +1,10 @@
 import numpy
 
-from alinea.astk.meteorology.sun_position import sun_position
+from alinea.astk.meteorology.sun_position import sun_position, \
+    sun_extraradiation
 from alinea.astk.meteorology.sun_position_astk import \
-    sun_position as sun_position_astk
+    sun_position as sun_position_astk, \
+    sun_extraradiation as sun_extraradiation_astk
 from alinea.astk.meteorology.sun_position_ephem import \
     sun_position as sun_position_ephem
 
@@ -20,3 +22,9 @@ def test_alternative_sun_position():
     numpy.testing.assert_allclose(sune, sun, rtol=0.01)
     # astk is equivalent <5 %
     numpy.testing.assert_allclose(suna, sun, rtol=0.05)
+
+
+def test_extra_radiation():
+    df = sun_extraradiation()
+    dfa = sun_extraradiation_astk()
+    numpy.testing.assert_allclose(dfa, df, rtol=0.01)
